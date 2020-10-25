e2258f1a3e9e59396f36c06ef682acf4aca1f96f
Jonas Schneider <jonas@jonas-linux.fritz.box>
2020-10-25 16:27:01 +0100

    started showdown implementation

--- hand/showdown.go
+++ hand/showdown.go
@@ -1,53 +1,70 @@
 package hand
 
-import "github.com/JohnnyS318/go-poker/models"
+import (
+	"github.com/JohnnyS318/go-poker/models"
+)
 
 func (h *Hand) showdown() {
+
+	if len(h.Players) < 1 {
+		return
+	}
 
 	if len(h.Players) == 1 {
 		//h.In[0] wins
 	}
+}
 
-	//
-	counts := make(map[string]int, len(h.Players))
+func (h *Hand) RankSpecificHand(cards []models.Card) uint16{
+	//Hand identifier explanation:
+	// 1 Byte Number:
+	// 4 MSB: Describe Hand State (0: High Card - 8: Straight Flush)
+	// 4 LSB: Describe State Identifier via Highest Card (0: Card Value 2 - 12: Card Value Ace)
+	// Hand Identifier ranges from 0b00000000 (Theoretically Highest Card Two) to 0b10011101 (Straight Flush with Ace -> Royal Flush)
+	// If a given Hand h1 is better than another h2, h1 > h2 always yields true.
+	identifier := normalizeAce(cards[0].Value)
 
-	for i, n := range h.Players {
-		hole := h.HoleCards[n.ID]
-		cards := append(hole[:], h.Board[:]...)
-		counts[n.ID] = h.RankPlayer(i, &n, cards[:7])
+	sCol := normalizeAce(cards[0].Color);
+    lP1 := -1;
+    lP2 := -1;
+	over  := -1
+	
+    //Possible 9 hand states are one by one excluded by masking a 9 bit number.
+	validStates := 0b111111111
+
+	for i := 0; i < 5; i++ {
+		open := normalizeAce(cards[i].Value)
+		r := open - identifier;
+		if r > 4 || r < 4{
+			validStates = validStates & 0b011101111
+		}
+		if open == identifier || open == lP1 || open == lp2 || open == over {
+			validStates = validStates &  0b011001110;
+		} else {
+			over = lP2
+			if open > identifier{
+                lP2 = lP1
+                lP1 = identifier
+                identifier = open
+			} else if open > lp1{
+				lP2 = lP1
+				lP1 = open
+			} else if open > lp2 {
+				
+			}
+		}
+
 	}
 
 }
 
-func (h *Hand) RankPlayer(i int, p *models.Player, cards []models.Card) int {
-
-	highestCard := -1
-	dColor := make(map[int]int, 4)
-
-	// k: card value
-	// v: count
-	dValue := make(map[int]int, 13)
-
-	for _, n := range cards {
-		count, ok := dValue[n.Value]
-		if ok {
-			dValue[n.Value] = count + 1
-		} else {
-			dValue[n.Value] = 1
-		}
-
-		count, ok = dColor[n.Color]
-		if ok {
-			dColor[n.Color] = count + 1
-		} else {
-			dColor[n.Color] = 1
-		}
-
-		if n.Value > highestCard {
-			highestCard = n.Value
-		}
+func normalizeAce(number int) int {
+	if number-1 < 0 {
+		return 12
 	}
-
-	// [1:2, 2:2, 3:0, 4:1, ...]
-	return 1
+	return number
 }
+
+func v(n int, c []models.Card) int {
+	return normalizeAce(c[n].Value)
+})
