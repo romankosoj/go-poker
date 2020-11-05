e57f71a9581dbf0b0564fb148793e82dcc5f769a
Jonas Schneider <jonas@jonas-linux.fritz.box>
2020-11-05 15:33:53 +0100

    added ToBeAdded list in lobby managing

--- lobby/lobby.go
+++ lobby/lobby.go
@@ -15,25 +15,29 @@ import (
 )
 
 type Lobby struct {
+	lock                 sync.RWMutex
 	LobbyID              string          `json:"lobbyId"`
 	Players              []models.Player `json:"players"`
 	GameStarted          bool
 	MinBuyIn             int
 	MaxBuyIn             int
 	Blinds               int
-	ToRemove             []int
+	ToBeRemoved          []int
+	ToBeAdded            []*models.Player
 	LobbyManagerCapacity map[string]int
 	PlayerQueue          []*models.Player
 	Bank                 *bank.Bank
+	dealer               int
 }
 
 func NewLobby() *Lobby {
 	return &Lobby{
 		LobbyID:     GenerateLobbyID(),
 		Players:     make([]models.Player, 0),
-		ToRemove:    make([]int, 0),
+		ToBeRemoved: make([]int, 0),
 		PlayerQueue: make([]*models.Player, 0),
 		Bank:        bank.NewBank(),
+		dealer:      -1,
 	}
 }
 
@@ -55,52 +59,77 @@ func (l *Lobby) DequeuePlayer() (player *models.Player, ok bool) {
 }
 
 func (l *Lobby) Start() {
+	l.lock.Lock()
 	l.GameStarted = true
+	l.lock.Unlock()
 	log.Printf("Lobby Started")
 	// SETUP
-	dealer := -1
-	var wg sync.WaitGroup
-	wg.Add(1)
 	go func() {
 		for len(l.Players) > 2 {
 			log.Printf("Game started")
 			time.Sleep(10 * time.Second)
 
 			rand.Seed(time.Now().UnixNano())
-			if dealer < 0 {
-				dealer = rand.Intn(len(l.Players))
+			if l.dealer < 0 {
+				l.dealer = rand.Intn(len(l.Players))
 			} else {
-				dealer = (dealer + 1) % len(l.Players)
+				l.dealer = (l.dealer + 1) % len(l.Players)
 			}
-			log.Printf("Dealer choosen random %d", dealer)
+			log.Printf("Dealer choosen random %d", l.dealer)
 
 			for i := range l.Players {
 				l.Players[i].Active = true
 			}
 
-			hand := hand.NewHand(l.Players, l.Bank, dealer)
+			hand := hand.NewHand(l.Players, l.Bank, l.dealer)
 
 			hand.Start()
 
+			l.lock.Lock()
 			l.GameStarted = false
-			if len(l.ToRemove) < 1 {
+			l.lock.Unlock()
+			if l.HasToBeRemoved() {
 				l.RemoveAfterGame()
 			}
+			if l.HasToBeRemoved() {
 
+				l.EmptyToBeAdded()
+			}
 		}
 	}()
-	wg.Wait()
+}
+
+func (l *Lobby) GetGameStarted() bool {
+	l.lock.RLock()
+	defer l.lock.RUnlock()
+	return l.GameStarted
+}
+
+func (l *Lobby) HasToBeAdded() bool {
+	l.lock.RLock()
+	defer l.lock.RUnlock()
+	return len(l.ToBeAdded) > 0
+}
+
+func (l *Lobby) HasToBeRemoved() bool {
+	l.lock.RLock()
+	defer l.lock.RUnlock()
+	return len(l.ToBeRemoved) > 0
 }
 
 func (l *Lobby) JoinPlayer(player *models.Player) {
-	if len(l.Players) <= 10 {
-		i := len(l.Players)
-		l.Players = append(l.Players, *player)
 
-		log.Printf("Player joined lobby %v", player)
-		log.Printf("Player count in join lobby %v", len(l.Players))
+	le := len(l.Players) + len(l.ToBeAdded)
+	if le <= 10 {
+		l.lock.Lock()
+		l.ToBeAdded = append(l.ToBeAdded, player)
+		l.lock.Unlock()
 
-		utils.SendToPlayer(player, events.NewJoinSuccessEvent(l.LobbyID, l.Players, l.GameStarted, 0, i, l.MaxBuyIn, l.MinBuyIn, l.Blinds))
+		if !l.GameStarted {
+			l.EmptyToBeAdded()
+		}
+
+		utils.SendToPlayer(player, events.NewJoinSuccessEvent(l.LobbyID, l.Players, l.GameStarted, 0, le, l.MaxBuyIn, l.MinBuyIn, l.Blinds))
 
 		l.Bank.AddPlayer(player)
 
@@ -110,6 +139,22 @@ func (l *Lobby) JoinPlayer(player *models.Player) {
 	} else {
 		l.EnqueuePlayer(player)
 	}
+}
+
+func (l *Lobby) EmptyToBeAdded() {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	for i := range l.ToBeAdded {
+		if len(l.Players) < 10 {
+			l.Players = append(l.Players, *l.ToBeAdded[i])
+			log.Printf("Player joined lobby %v", l.ToBeAdded[i])
+			log.Printf("Player count in join lobby %v", len(l.Players))
+		} else {
+			l.EnqueuePlayer(l.ToBeAdded[i])
+		}
+	}
+	l.ToBeAdded = nil
+
 }
 
 func (l *Lobby) RemovePlayerByID(id string) error {
@@ -124,31 +169,21 @@ func (l *Lobby) RemovePlayerByID(id string) error {
 
 	log.Printf("Game runing ?: [%v]", l.GameStarted)
 
-	err := l.RemovePlayer(i)
+	l.RemovePlayer(i)
 
 	log.Printf("Players in lobby? %v", len(l.Players))
 
-	return err
+	return nil
 }
 
-func (l *Lobby) RemovePlayer(i int) error {
-	if l.GameStarted {
-		l.ToRemove = append(l.ToRemove, i)
-	} else {
-		//Keep order so that the next dealer is choosen correctly.
-		l.Players = append(l.Players[:i], l.Players[i+1:]...)
-
-		player, ok := l.DequeuePlayer()
-		if ok {
-			l.JoinPlayer(player)
-		}
-
-		// Non blocking channel send (possibly nobody was listening for a player leaves and we have to continue)
-		//select {
-		//case l.PlayerLeavesChannel <- l.LobbyID:
-		//}
+func (l *Lobby) RemovePlayer(i int) {
+	l.lock.Lock()
+	l.ToBeRemoved = append(l.ToBeRemoved, i)
+	gameStarted := l.GameStarted
+	l.lock.Unlock()
+	if !gameStarted {
+		l.RemoveAfterGame()
 	}
-	return nil
 }
 
 func (l *Lobby) FindPlayerByID(id string) int {
@@ -166,7 +201,9 @@ func (l *Lobby) HasCapacaty() bool {
 
 //RemoveAfterGame removes the left players from the lobby after a game has finished. During a game the player is counted as folded.
 func (l *Lobby) RemoveAfterGame() {
-	for _, i := range l.ToRemove {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	for _, i := range l.ToBeRemoved {
 		l.Bank.RemovePlayer(l.Players[i].ID)
 		l.Players = append(l.Players[:i], l.Players[i+1:]...)
 		player, ok := l.DequeuePlayer()
